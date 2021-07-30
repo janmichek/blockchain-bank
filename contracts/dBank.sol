976f8c64b30e1242e8aa2b141806f19fef346cc8
Jan Michek <jan@janmichek.cz>
2021-07-30 08:14:23 +0200

    add frontend

--- contracts/dBank.sol
+++ contracts/dBank.sol
@@ -9,15 +9,11 @@ contract dBank {
 
     mapping(address => uint) public depositStart;
     mapping(address => uint) public etherBalanceOf;
-    mapping(address => uint) public collateralEther;
 
     mapping(address => bool) public isDeposited;
-    mapping(address => bool) public isBorrowed;
 
     event Deposit(address indexed user, uint etherAmount, uint timeStart);
     event Withdraw(address indexed user, uint etherAmount, uint depositTime, uint interest);
-    event Borrow(address indexed user, uint collateralEtherAmount, uint borrowedTokenAmount);
-    event PayOff(address indexed user, uint fee);
 
 //    constructor(Token _token) public {
     constructor(Token _token) {
@@ -65,42 +61,5 @@ contract dBank {
         isDeposited[msg.sender] = false;
 
         emit Withdraw(msg.sender, userBalance, depositTime, interest);
-    }
-
-    function borrow() payable public {
-        require(msg.value >= 1e16, 'Error, collateral must be >= 0.01 ETH');
-        require(isBorrowed[msg.sender] == false, 'Error, loan already taken');
-
-        //this Ether will be locked till user payOff the loan
-        collateralEther[msg.sender] = collateralEther[msg.sender] + msg.value;
-
-        //calc tokens amount to mint, 50% of msg.value
-        uint tokensToMint = collateralEther[msg.sender] / 2;
-
-        //mint&send tokens to user
-        token.mint(msg.sender, tokensToMint);
-
-        //activate borrower's loan status
-        isBorrowed[msg.sender] = true;
-
-        emit Borrow(msg.sender, collateralEther[msg.sender], tokensToMint);
-    }
-
-    function payOff() public {
-        require(isBorrowed[msg.sender] == true, 'Error, loan not active');
-        require(token.transferFrom(msg.sender, address(this), collateralEther[msg.sender] / 2), "Error, can't receive tokens");
-        //must approve dBank 1st
-
-        uint fee = collateralEther[msg.sender] / 10;
-        //calc 10% fee
-
-        //send user's collateral minus fee
-        msg.sender.transfer(collateralEther[msg.sender] - fee);
-
-        //reset borrower's data
-        collateralEther[msg.sender] = 0;
-        isBorrowed[msg.sender] = false;
-
-        emit PayOff(msg.sender, fee);
     }
 }
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,112 @@
 <template>
   <div id="app">
-   <header></header>
-    <div v-else>
-      Loading
-    </div>
+    <header>
+      Account: {{ account }}
+      <br>
+      Balance: {{ web3.utils.fromWei(balance, 'ether') }}
+    </header>
+    <section>
+      <h2>Deposit</h2>
+      How much do you want to deposit?
+      <br><br>
+      (min. amount is 0.01 ETH)
+      <br><br>
+
+      <input
+        v-model="depositAmount"
+        step="0.01"
+        type='number'>
+
+      <button @click="deposit">DEPOSIT</button>
+
+    </section>
+    <hr>
+    <section>
+      <h2>Withdraw</h2>
+
+      Do you want to withdraw + take interest
+      <br><br>
+
+      <button @click="withdraw">WITHDRAW</button>
+    </section>
   </div>
 </template>
 
 <script>
+  import dBank from './contracts/dBank.json'
+  import Token from './contracts/Token.json'
+  import Web3 from 'web3'
+
+
   export default {
     name: 'app',
+    data () {
+      return {
+        web3: undefined,
+        account: '',
+        token: null,
+        dbank: null,
+        balance: 0,
+        dBankAddress: null,
+        depositAmount: 0,
+      }
+
+    },
+    async mounted () {
+      await this.loadBlockchainData()
+    },
+    methods: {
+      async loadBlockchainData () {
+        if (typeof window.ethereum !== 'undefined') {
+          this.web3 = new Web3(window.ethereum)
+          const netId = await this.web3.eth.net.getId()
+          const accounts = await this.web3.eth.getAccounts()
+          this.account = accounts[0]
+          //load balance
+          if (typeof accounts[0] !== 'undefined') {
+            this.balance = await this.web3.eth.getBalance(accounts[0])
+          } else {
+            window.alert('Please login with MetaMask')
+          }
+
+          //load contracts
+          try {
+            this.token = new this.web3.eth.Contract(Token.abi, Token.networks[netId].address)
+            this.dbank = new this.web3.eth.Contract(dBank.abi, dBank.networks[netId].address)
+            this.dBankAddress = dBank.networks[netId].address
+          } catch (e) {
+            /* eslint-disable no-console*/
+
+            console.log('Error', e)
+            window.alert('Contracts not deployed to the current network')
+          }
+        } else {
+          window.alert('Please install MetaMask')
+        }
+      },
+
+      async deposit () {
+        if (this.dbank !== 'undefined') {
+          const amount = this.web3.utils.toWei(this.depositAmount, 'ether')
+          try {
+            await this.dbank.methods.deposit().send({ value: amount, from: this.account })
+          } catch (e) {
+            console.log('Error, deposit: ', e)
+          }
+        }
+      },
+
+      async withdraw () {
+        if (this.dbank !== 'undefined') {
+          try {
+            await this.dbank.methods.withdraw().send({ from: this.account })
+          } catch (e) {
+            console.log('Error, withdraw: ', e)
+          }
+        }
+      },
+
+    },
   }
 </script>
 
